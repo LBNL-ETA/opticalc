8c516bd9eeb235936c562ddb0fba045d385abf24
StephenCzarnecki <sczarnecki@gmail.com>
2021-08-16 19:25:27 -0400

    Initial commit for moving functionality into new repo.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+setup(
+    name='opticalc',
+    version='0.0.1',
+    long_description='',
+    # tell setuptools to look for any packages under 'src'
+    packages=find_packages(where='src'),
+    # tell setuptools that all packages will be under the 'src' directory
+    # and nowhere else
+    package_dir={'': 'src'},
+    install_requires=['pydantic>=1.8.2',
+                      'py_igsdb_optical_data @ git+https://github.com/LBNL-ETA/py_igsdb_optical_data',
+                      'pywincalc @ git+https://github.com/danielmcquillen/pywincalc@revert'],
+    test_suite='tests',
+    zip_safe=False,
+)
