b1c3b692a20e52d47cb0e7f45193812057bb661e
Daniel McQuillen <daniel@mcquilleninteractive.com>
2022-02-08 11:25:30 +1100

    Target v2.3.0, the latest stable version of pywincalc

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # and nowhere else
     package_dir={'': 'src'},
     install_requires=['pydantic>=1.9.0',
-                      'py_igsdb_optical_data @ git+https://github.com/LBNL-ETA/py_igsdb_optical_data@main',
+                      'py_igsdb_optical_data @ git+https://github.com/LBNL-ETA/py_igsdb_optical_data@v2.3.0',
                       'pywincalc @ git+https://github.com/LBNL-ETA/pywincalc@develop'],
     test_suite='tests',
     zip_safe=False,
