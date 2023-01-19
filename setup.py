17158a6ad107d1fbef55179086f14311509f5fd7
Daniel McQuillen <daniel@mcquilleninteractive.com>
2023-01-19 16:39:35 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opticalc',
-    version='0.0.9',
+    version='0.0.10',
     long_description='',
     # tell setuptools to look for any packages under 'src'
     packages=find_packages(where='src'),
