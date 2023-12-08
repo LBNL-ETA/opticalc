c48dd3af7ffb191d46468c5efa478d21b330ee8b
Daniel McQuillen <daniel@mcquilleninteractive.com>
2023-12-08 11:35:50 +1100

    Update dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     package_dir={'': 'src'},
     install_requires=['pydantic>=1.9.0',
                       'py_igsdb_base_data@git+https://github.com/LBNL-ETA/py_igsdb_base_data@main',
-                      'pywincalc==v2.4.3'],
+                      'pywincalc==v3.3.1'],
     test_suite='tests',
     zip_safe=False,
 )
