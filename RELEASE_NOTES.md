82ebc8481ac518876628a05aef487953ed72f409
Daniel McQuillen <daniel@mcquilleninteractive.com>
2024-03-25 17:25:26 +1100

    Update version and release notes

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,9 @@
 Release Notes:
 
+0.0.21
+
+- Do not call pywincalc calc_thermal_ir if IR wavelength data is not present.
+
 0.0.20
 
 - Updates py_igsdb_base_data to 0.0.51
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opticalc',
-    version='0.0.20',
+    version='0.0.21',
     long_description='',
     # tell setuptools to look for any packages under 'src'
     packages=find_packages(where='src'),
