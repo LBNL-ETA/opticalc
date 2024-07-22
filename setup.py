41772e7ad555fd6ab733ab409b6768bef865a908
Daniel McQuillen <daniel@mcquilleninteractive.com>
2024-07-22 14:20:34 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # tell setuptools that all packages will be under the 'src' directory
     # and nowhere else
     package_dir={'': 'src'},
-    install_requires=['pydantic>=1.9.0',
+    install_requires=['pydantic>=2.8.2',
                       'py_igsdb_base_data@git+https://github.com/LBNL-ETA/py_igsdb_base_data@main',
                       'pywincalc==v3.3.1'],
     test_suite='tests',
