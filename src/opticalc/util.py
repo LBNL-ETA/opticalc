716de808de4043b2c364c269b67b924c8567c328
Daniel McQuillen <daniel@mcquilleninteractive.com>
2025-04-04 12:01:37 +1100

    Update convert_wavelength_data

--- src/opticalc/util.py
+++ src/opticalc/util.py
@@ -93,13 +93,17 @@ def convert_wavelength_data(
             )
             diffuse_measurements = individual_wavelength_measurement.get("diffuse", {})
 
-        # Make sure empty strings are converted to None
+        # Convert values
         for key in specular_measurements:
             if specular_measurements[key] == "":
                 specular_measurements[key] = None
+            if specular_measurements[key]:
+                specular_measurements[key] = float(specular_measurements[key])
         for key in diffuse_measurements:
-            if diffuse_measurements[key] == "":
-                diffuse_measurements[key] = None
+            if diffuse_measurements[key] == "" or diffuse_measurements[key] is None:
+                diffuse_measurements[key] = 0
+            if diffuse_measurements[key]:
+                diffuse_measurements[key] = float(diffuse_measurements[key])
 
         if combine_diffuse_and_specular:
             pywincalc_direct_component = pywincalc.OpticalMeasurementComponent(
