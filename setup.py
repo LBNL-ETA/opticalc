95728aa98b8d6f716564ab427579fb41823be49c
Daniel McQuillen <daniel@mcquilleninteractive.com>
2026-05-12 08:41:27 +1000

    bump dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opticalc",
-    version="0.0.35",
+    version="0.0.36",
     long_description="",
     # tell setuptools to look for any packages under 'src'
     packages=find_packages(where="src"),
@@ -10,7 +10,7 @@ setup(
     # and nowhere else
     package_dir={"": "src"},
     install_requires=[
-        "pydantic>=2.8.2",
+        "pydantic>=2.13.4",
         "py_igsdb_base_data@git+https://github.com/LBNL-ETA/py_igsdb_base_data@main",
         "pywincalc==3.8.2",
     ],
